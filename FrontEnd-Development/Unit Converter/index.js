62792fe45ce949a518cd376ae4ee3b229a2fe52d
Rodas Samson Gebrtensea <rsamson139@gmail.com>
2025-07-29 18:49:10 +0100

    Function Created + bugs

--- FrontEnd-Development/Unit Converter/index.js
+++ FrontEnd-Development/Unit Converter/index.js
@@ -1,5 +1,20 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+// conversion rates to be used
+// 1 meter = 3.281 feet
+// 1 liter = 0.264 gallon
+// 1 kilogram = 2.204 pound
+
+
+// First get the ids from HTML
+const Length = document.getElementById("Length")
+const Volume = document.getElementById("Volume")
+const Mass = document.getElementById("Mass")
+
+// Get the id from the user input field
+const Conversion = document.getElementById("Conversion").Value
+
+// Function for Length , Volume and Mass
+function Convert(){
+    let a = Conversion * 3.28
+    // a = Length.textContent
+    console.log(12)
+}
