3928280c0f8ba8d230260ec3046a291cd79897c1
Rodas Samson Gebrtensea <rsamson139@gmail.com>
2025-08-05 18:20:47 +0100

    JS Completed + Bug Fixes

--- FrontEnd-Development/Unit Converter/index.js
+++ FrontEnd-Development/Unit Converter/index.js
@@ -10,11 +10,32 @@ const Volume = document.getElementById("Volume")
 const Mass = document.getElementById("Mass")
 
 // Get the id from the user input field
-const Conversion = document.getElementById("Conversion").Value
 
 // Function for Length , Volume and Mass
 function Convert(){
-    let a = Conversion * 3.28
-    // a = Length.textContent
-    console.log(12)
+    const Conversion = parseFloat(document.getElementById("Conversion").value)
+    
+    if(isNaN(Conversion)){
+        alert("Please enter a valid number!")
+        return
+    }
+
+    const metersToFeet = (Conversion * 3.281).toFixed(3);
+    const feetToMeters = (Conversion / 3.281).toFixed(3);
+
+    document.getElementById('Length').innerText =
+        `${Conversion} meters = ${metersToFeet} feet | ${Conversion} feet = ${feetToMeters} meters`;
+
+    const litreToGallons = (Conversion * 0.264).toFixed(3);
+    const gallonToLitres = (Conversion / 0.264).toFixed(3);
+
+    document.getElementById('Volume').innerText =
+        `${Conversion} litres = ${litreToGallons} gallon | ${Conversion} gallon = ${gallonToLitres} litres`;
+
+    const kilogramToPounds = (Conversion * 2.204).toFixed(3);
+    const poundsToKilograms = (Conversion / 2.204).toFixed(3);
+
+    document.getElementById('Mass').innerText =
+        `${Conversion} Kilograms = ${kilogramToPounds} pounds | ${Conversion} pound = ${poundsToKilograms} Kilograms`;
+
 }
