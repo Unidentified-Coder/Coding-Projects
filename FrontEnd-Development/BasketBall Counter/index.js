365031088235017b6ac5fdb4563a1da4fd996d47
Rodas Samson Gebrtensea <rsamson139@gmail.com>
2025-08-13 10:02:16 +0100

    JavaScript Functionality added

--- FrontEnd-Development/BasketBall Counter/index.js
+++ FrontEnd-Development/BasketBall Counter/index.js
@@ -1,36 +1,16 @@
 let home = 0
 let guest = 0
 
-document.getElementById("home_score").textContent = home
-document.getElementById("guest_score").textContent = guest
+document.getElementById("H_Score").textContent = home
+document.getElementById("A_Score").textContent = guest
 
-function add_one(){
-    home += 1
-    document.getElementById("home_score").textContent = home
+function home_add(pass){
+    home += pass
+    document.getElementById("H_Score").textContent = home
 
 }
 
-function mid_range(){
-    home += 2
-    document.getElementById("home_score").textContent = home
-}
-
-function from_deep(){
-    home += 3
-    document.getElementById("home_score").textContent = home
-}
-
-function free_throw(){
-    guest += 1
-    document.getElementById("guest_score").textContent = guest
-}
-
-function from_2(){
-    guest += 2
-    document.getElementById("guest_score").textContent = guest
-}
-
-function from_3(){
-    guest += 3
-    document.getElementById("guest_score").textContent = guest
+function away_add(pass){
+    guest += pass
+    document.getElementById("A_Score").textContent = guest
 }
